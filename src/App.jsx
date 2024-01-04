f8f8e97df128b0bf8942a41d752e4617abfca171
fadayopaul <paulfadayo@yahoo.com>
2024-01-04 11:21:36 +0100

    chore: :lipstick: Global Styles with styled component

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,16 @@
+import styled from "styled-components";
+
+const H1 = styled.h1`
+  font-size: 30px;
+  font-weight: 600;
+`;
+
 function App() {
-  return <div>Hello</div>;
+  return (
+    <div>
+      <H1>The Doze Haven</H1>
+    </div>
+  );
 }
 
 export default App;
--- src/styles/GlobalStyles.js
+++ src/styles/GlobalStyles.js
@@ -1,18 +1,7 @@
-/*
-<link rel="preconnect" href="https://fonts.googleapis.com" />
-<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
-<link
-  href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;500;600;700&display=swap"
-  rel="stylesheet"
-/>
-<link
-  href="https://fonts.googleapis.com/css2?family=Sono:wght@400;500;600&display=swap"
-  rel="stylesheet"
-/>
-*/
+import { createGlobalStyle } from "styled-components";
 
 /* Colors adapted from https://tailwindcss.com/docs/customizing-colors */
-
+const GlobalStyles = createGlobalStyle`
 :root {
   /* Indigo */
   --color-brand-50: #eef2ff;
@@ -154,6 +143,22 @@ img {
   /* For dark mode */
   filter: grayscale(var(--image-grayscale)) opacity(var(--image-opacity));
 }
+`;
+
+export default GlobalStyles;
+
+/*
+<link rel="preconnect" href="https://fonts.googleapis.com" />
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+<link
+  href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;500;600;700&display=swap"
+  rel="stylesheet"
+/>
+<link
+  href="https://fonts.googleapis.com/css2?family=Sono:wght@400;500;600&display=swap"
+  rel="stylesheet"
+/>
+*/
 
 /*
 FOR DARK MODE
