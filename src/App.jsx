f45466b4ae288980ca928460cc8a3b2cae526731
fadayopaul <paulfadayo@yahoo.com>
2024-01-04 16:47:15 +0100

    chore: :building_construction: global styles

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,33 @@
 import styled from "styled-components";
+import GlobalStyles from "./styles/GlobalStyles";
+import { Button } from "./ui/Button";
+import Input from "./ui/Input";
 
 const H1 = styled.h1`
   font-size: 30px;
   font-weight: 600;
+  background-color: yellow;
+`;
+
+const StyledApp = styled.main`
+  background-color: orangered;
+  padding: 20px;
 `;
 
 function App() {
   return (
-    <div>
-      <H1>The Doze Haven</H1>
-    </div>
+    <>
+      <GlobalStyles />
+      <StyledApp>
+        <H1>The Doze Haven</H1>
+
+        <Button onClick={() => alert("Right Clicked")}>Right</Button>
+        <Button onClick={() => alert("Left Clicked")}>Left</Button>
+
+        <Input placeholder="Number of guest" />
+        <Input placeholder="Number of guest" />
+      </StyledApp>
+    </>
   );
 }
 
--- src/styles/GlobalStyles.js
+++ src/styles/GlobalStyles.js
@@ -148,19 +148,6 @@ img {
 export default GlobalStyles;
 
 /*
-<link rel="preconnect" href="https://fonts.googleapis.com" />
-<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
-<link
-  href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;500;600;700&display=swap"
-  rel="stylesheet"
-/>
-<link
-  href="https://fonts.googleapis.com/css2?family=Sono:wght@400;500;600&display=swap"
-  rel="stylesheet"
-/>
-*/
-
-/*
 FOR DARK MODE
 
 --color-grey-0: #18212f;
