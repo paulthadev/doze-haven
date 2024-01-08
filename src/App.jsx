a6c6f2d7c78bee89d534c00e03d4fafc52ec2936
fadayopaul <paulfadayo@yahoo.com>
2024-01-08 18:06:01 +0100

    chore: :zap: setting up pages and routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,47 +1,37 @@
-import styled from "styled-components";
+import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
 import GlobalStyles from "./styles/GlobalStyles";
 
-import { Button } from "./ui/Button";
-import Input from "./ui/Input";
-import Heading from "./ui/Heading";
-import Row from "./ui/Row";
-
-const StyledApp = styled.main`
-  padding: 20px;
-`;
+import Dashboard from "./pages/Dashboard";
+import Bookings from "./pages/Bookings";
+import Cabins from "./pages/Cabins";
+import Users from "./pages/Users";
+import Settings from "./pages/Settings";
+import Account from "./pages/Account";
+import Login from "./pages/Login";
+import PageNotFound from "./pages/PageNotFound";
 
 function App() {
   return (
     <>
-      <GlobalStyles />
-      <StyledApp>
-        <Row>
-          <Row type="horizontal">
-            <Heading as="h1">The Doze Haven</Heading>
+      <GlobalStyles /> {/* Styling that applys to the entire App */}
+      <BrowserRouter>
+        <Routes>
+          {/* index page
+        The replace prop on the Navigate component is used to render a declarative redirect. It allows us to navigate back by replacing the current element in the history stack.*/}
+          <Route index element={<Navigate replace to="dashboard" />} />
 
-            <div>
-              <Heading as="h2">Check in and out</Heading>
-              <Button onClick={() => alert("checked in")}>Check In</Button>
-              <Button
-                variation="secondary"
-                size="small"
-                onClick={() => alert("checked out")}
-              >
-                Check Out
-              </Button>
-            </div>
-          </Row>
+          <Route path="dashboard" element={<Dashboard />} />
+          <Route path="bookings" element={<Bookings />} />
+          <Route path="cabins" element={<Cabins />} />
+          <Route path="users" element={<Users />} />
+          <Route path="settings" element={<Settings />} />
+          <Route path="account" element={<Account />} />
+          <Route path="login" element={<Login />} />
 
-          <Row>
-            <Heading as="h3">Form</Heading>
-
-            <form>
-              <Input placeholder="Number of guest" />
-              <Input placeholder="Number of guest" />
-            </form>
-          </Row>
-        </Row>
-      </StyledApp>
+          {/* Page to be matched if none of the urls can be matched  */}
+          <Route path="*" element={<PageNotFound />} />
+        </Routes>
+      </BrowserRouter>
     </>
   );
 }
