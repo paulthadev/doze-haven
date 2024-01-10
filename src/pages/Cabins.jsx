3e30f189b10804db4fa36b90a419f719cf5fa8a4
fadayopaul <paulfadayo@yahoo.com>
2024-01-10 17:39:58 +0100

    chore: :white_check_mark: fetch and update cabin data form the remote server

--- src/pages/Cabins.jsx
+++ src/pages/Cabins.jsx
@@ -1,21 +1,19 @@
-import { useEffect } from "react";
 import Heading from "../ui/Heading";
 import Row from "../ui/Row";
-import { getCabins } from "../services/apiCabins";
+import CabinTable from "../features/cabins/CabinTable";
 
 function Cabins() {
-  useEffect(function () {
-    //fetch the data from supabase through the apiCabins()
-    //
-    getCabins().then((data) => console.log(data));
-  }, []);
-
   return (
-    <Row type="horizontal">
-      <Heading as="h1">All cabins</Heading>
-      <p>TEST</p>
-      <img src="https://spukeonlyqtisnkqhveh.supabase.co/storage/v1/object/public/cabin-images/cabin-001.jpg" />
-    </Row>
+    <>
+      <Row type="horizontal">
+        <Heading as="h1">All cabins</Heading>
+        <p>filter/sort</p>
+      </Row>
+
+      <Row>
+        <CabinTable />
+      </Row>
+    </>
   );
 }
 
