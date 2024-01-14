0e98523e444e2d7b26a6660b1f12d6468e3a97fa
fadayopaul <paulfadayo@yahoo.com>
2024-01-14 07:59:01 +0100

    refactor: :white_check_mark: named export server url

--- src/services/supabase.js
+++ src/services/supabase.js
@@ -1,6 +1,6 @@
 import { createClient } from "@supabase/supabase-js";
 
-const supabaseUrl = "https://spukeonlyqtisnkqhveh.supabase.co";
+export const supabaseUrl = "https://spukeonlyqtisnkqhveh.supabase.co";
 
 const supabaseKey =
   "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InNwdWtlb25seXF0aXNua3FodmVoIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MDQ3NjE1NDMsImV4cCI6MjAyMDMzNzU0M30.eg7bkc4ZIg4edPKhYcF-SPC7KT4XEWWvBlTVl7x8qgU";
