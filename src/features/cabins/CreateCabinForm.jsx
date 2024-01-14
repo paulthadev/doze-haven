ed7efbd80cc15ba49a57342908d63940596643a3
fadayopaul <paulfadayo@yahoo.com>
2024-01-14 08:07:23 +0100

    feat: :sparkles: upload image data to the server and fix discount data type

--- src/features/cabins/CreateCabinForm.jsx
+++ src/features/cabins/CreateCabinForm.jsx
@@ -44,7 +44,7 @@ function CreateCabinForm() {
   });
 
   function onSubmit(data) {
-    mutate(data);
+    mutate({ ...data, image: data.image[0] });
   }
 
   return (
@@ -100,7 +100,7 @@ function CreateCabinForm() {
           {...register("discount", {
             required: "This field is required",
             validate: (value) =>
-              value <= getValues()?.regularPrice ||
+              Number(value) <= Number(getValues()?.regularPrice) ||
               "Discount should be less than regular price",
           })}
         />
@@ -121,7 +121,12 @@ function CreateCabinForm() {
       </FormRow>
 
       <FormRow label={"Cabin photo"}>
-        <FileInput id="image" accept="image/*" {...register("image")} />
+        <FileInput
+          id="image"
+          accept="image/*"
+          disabled={isCreating}
+          {...register("image")}
+        />
       </FormRow>
 
       <FormRow>
