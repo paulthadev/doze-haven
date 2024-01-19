e21355842150ac9397c31407c7a5e3787b3894be
fadayopaul <paulfadayo@yahoo.com>
2024-01-19 01:23:37 +0100

    refactor: :zap: abstracting React Query CreateCabin & EditCabin into custom hook

---
+++ src/features/cabins/useEditCabin.js
@@ -0,0 +1,22 @@
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+import toast from "react-hot-toast";
+import { createEditCabin } from "../../services/apiCabins";
+
+export function useEditCabin() {
+  const queryClient = useQueryClient();
+
+  const { mutate: editCabin, isLoading: isEdititng } = useMutation({
+    mutationFn: ({ newCabinData, id }) => createEditCabin(newCabinData, id),
+
+    onSuccess: () => {
+      toast.success("cabin edited successfully!");
+
+      queryClient.invalidateQueries({
+        queryKey: ["cabins"],
+      });
+    },
+    onError: (error) => toast.error(error.message),
+  });
+
+  return { editCabin, isEdititng };
+}
