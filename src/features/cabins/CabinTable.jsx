f1dc960801f5efabc7e0026ef7c5c684953beed2
fadayopaul <paulfadayo@yahoo.com>
2024-06-06 22:46:02 +0100

    feat: :sparkles: sort cabins

--- src/features/cabins/CabinTable.jsx
+++ src/features/cabins/CabinTable.jsx
@@ -17,6 +17,8 @@ function CabinTable() {
 
   if (isLoading) return <Spinner />;
 
+  // FILTER
+
   // FETCHING the stored data in the URL and assigning it to the 'filterValue'
   const filterValue = searchParams.get("discount") || "all";
 
@@ -34,6 +36,14 @@ function CabinTable() {
   if (filterValue === "with-discount")
     filteredCabins = cabins.filter((cabin) => cabin.discount > 0);
 
+  // 2) SORT
+  const sortBy = searchParams.get("sortBy") || "startDate-asc";
+  const [field, direction] = sortBy.split("-");
+  const modifier = direction === "asc" ? 1 : -1;
+  const sortedCabins = filteredCabins.sort(
+    (a, b) => (a[field] - b[field]) * modifier
+  );
+
   return (
     <Menus>
       <Table columns="0.6fr 1.8fr 2.2fr 1fr 1fr 1fr">
@@ -48,7 +58,7 @@ function CabinTable() {
 
         <Table.Body
           // data={cabins}
-          data={filteredCabins}
+          data={sortedCabins}
           render={(cabin) => <CabinRow cabin={cabin} key={cabin.id} />}
         />
       </Table>
